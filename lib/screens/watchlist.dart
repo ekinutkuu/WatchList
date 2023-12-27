ea5944390a80b8848a04b320c7acc75669e0af5d
Ekin Utku <ekinutkugulmez@gmail.com>
2023-12-27 22:15:52 +0300

    added: watchlist design

--- lib/screens/watchlist.dart
+++ lib/screens/watchlist.dart
@@ -16,6 +16,9 @@ class _WatchlistState extends State<Watchlist> {
 
   @override
   Widget build(BuildContext context) {
+
+    double deviceWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
       /* sidebar.dart */
       drawer: SideBar(activePage: "Watchlist"),
@@ -37,13 +40,45 @@ class _WatchlistState extends State<Watchlist> {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            Row(
-              children: [
-                Container(
-
-                ),
-              ],
+            SizedBox(height: 20.0,),
+            Container(
+              width: deviceWidth,
+              height: 150.0,
+              color: PaleBlue,
+              child: Row(
+                children: [
+                  SizedBox(width: deviceWidth*0.03,),
+                  Container(
+                    width: deviceWidth*0.20,
+                    height: 100,
+                    color: LightBlue,
+                    child: Text("Photo", style: TextStyle(color: DarkBlue),),
+                  ),
+                  SizedBox(width: deviceWidth*0.03,),
+                  Container(
+                    width: deviceWidth*0.25,
+                    height: 30.0,
+                    color: LightBlue,
+                    child: Text("Name", style: TextStyle(color: DarkBlue),),
+                  ),
+                  SizedBox(width: deviceWidth*0.05,),
+                  Container(
+                    width: deviceWidth*0.23,
+                    height: 30.0,
+                    color: LightBlue,
+                    child: Text("+ 24/36", style: TextStyle(color: DarkBlue),),
+                  ),
+                  SizedBox(width: deviceWidth*0.05,),
+                  Container(
+                    width: deviceWidth*0.13,
+                    height: 25.0,
+                    color: LightBlue,
+                    child: Text("Edit", style: TextStyle(color: DarkBlue),),
+                  ),
+                ],
+              ),
             ),
+
           ],
         ),
       ),
